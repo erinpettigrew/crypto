db0c1ba2fd2c0d9e6bd88e644f9af321e7bdeb93
Matthew Fong <matthew.fong@gmail.com>
2015-10-23 10:42:56 -0400

    Add reviews scaffold and style new review form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022222124) do
+ActiveRecord::Schema.define(version: 20151023143609) do
 
   create_table "products", force: :cascade do |t|
     t.string   "product_brand"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151022222124) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
