c48981c9973caa361288471c5ca9cc7061ae168d
Erin Pettigrew <erin.pettigrew@gmail.com>
2015-12-01 18:42:23 -0500

    Continue improving category dropdown on product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -110,6 +110,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:product_brand, :product_name, :image)
+      params.require(:product).permit(:product_brand, :product_name, :image, :category_id)
     end
 end
