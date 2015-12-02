2ba85269dce23d7ef9fcacf2182ddb56510c873b
Erin Pettigrew <erin.pettigrew@gmail.com>
2015-12-01 23:40:08 -0500

    Format and make usable the category index and show pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,21 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-        @products = Product.where(category_id: @category.id).order("created_at DESC") 
+    @products = Product.where(category_id: @category.id).order("created_at DESC") 
+
+    @avg_rating = []
+    @review_count = []
+
+    for singleproduct in @products
+      @reviews = Review.where(product_id: singleproduct.id)
+      if @reviews.blank?
+        @avg_rating << 0
+        @review_count << 0
+      else
+        @avg_rating << @reviews.average(:rating).round(2) 
+        @review_count << @reviews.size
+      end
+    end
   end
 
 
