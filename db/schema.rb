c8b3f5e402a191be53782409d033c886764bb2a0
Erin Pettigrew <erin.pettigrew@gmail.com>
2015-12-02 22:42:06 -0500

    Add friendlyID to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201201037) do
+ActiveRecord::Schema.define(version: 20151203033130) do
 
   create_table "categories", force: :cascade do |t|
     t.text     "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "slug"
   end
+
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
