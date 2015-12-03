9fffb0d6461db8a8e17cf1d92fbd5fd525aae4ba
Erin Pettigrew <erin.pettigrew@gmail.com>
2015-12-03 00:05:35 -0500

    Re-order category page by most recently updated product

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @products = Product.where(category_id: @category.id).order("created_at DESC") 
+    @products = Product.where(category_id: @category.id).order("updated_at DESC") 
 
     @avg_rating = []
     @review_count = []
