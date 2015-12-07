5b1a6ed646af4be484a12f2c38f8b32939a753a1
Erin Pettigrew <erin.pettigrew@gmail.com>
2015-12-06 19:36:11 -0500

    Troubleshoot wrong review showing next to products on front page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,8 +38,8 @@ class ProductsController < ApplicationController
           @avg_rating << @reviews.average(:rating).round(2) 
           @review_count << @reviews.size
           @new << false
-          @review << @reviews.first
-          @user << @reviews.first.user
+          @review << @reviews.last
+          @user << @reviews.last.user
 
         end
 
