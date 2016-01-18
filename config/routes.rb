379d2865fcd10611d77dcbc52a5d9087f9425137
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-01-18 00:10:41 -0500

    Make feed the homepage and fix copy so it makes sense

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   # get 'pages/contact'
   #set homepage as product index list page
-  root 'categories#index'
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
