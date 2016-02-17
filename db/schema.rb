32c5cabc20ff1483afb8362c636b9d64106fab67
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-02-16 22:29:16 -0500

    Fix database schema from merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160208024426) do
-=======
 ActiveRecord::Schema.define(version: 20160216194250) do
->>>>>>> 03b946eed706941937f2feba264a8358c30d85e5
 
   create_table "applicants", force: :cascade do |t|
     t.string   "email"
