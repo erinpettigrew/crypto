ae737ef9685a20f7a08afc9e7ca63adb3aa78f58
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-02-23 15:19:10 -0500

    Remove development flag for SSL error silencer for mixpanel troubleshooting

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,9 +1,9 @@
 require 'mixpanel-ruby'
 
-if Rails.env.development?
+#if Rails.env.development?
 	#silence local SSL errors
 	Mixpanel.config_http do |http|
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	end
-end
+#end
 
