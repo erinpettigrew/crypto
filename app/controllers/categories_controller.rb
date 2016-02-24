c0207a0038cb8ac59b997eb3f81d69bf8a56f49f
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-02-23 19:44:11 -0500

    Sort products on category page by most recently created

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @products = @category.products.includes(:reviews, :uses)
+    @products = @category.products.includes(:reviews, :uses).order('created_at DESC')
     @avg_rating = []
     @review_count = []
     @total_uses = []
