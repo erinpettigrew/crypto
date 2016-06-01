cee6de1e5c1e8c572f2e1c811734529c882a280d
erinpettigrew <erin.pettigrew@gmail.com>
2016-06-01 19:38:43 -0400

    Clean up posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,17 +7,13 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -33,8 +29,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -47,8 +41,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -58,19 +50,17 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:user_id, :title, :image, :excerpt, :url)
-    end
+  def post_params
+    params.require(:post).permit(:user_id, :title, :image, :excerpt, :url)
+  end
 
-    def check_user
-      unless current_user.admin?
-        redirect_to root_url, alert: "Sorry, only admins can do that!"
-      end
+  def check_user
+    unless current_user.admin?
+      redirect_to root_url, alert: "Sorry, only admins can do that!"
     end
+  end
 end
