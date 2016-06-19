1d796ee58b5b1640bc4f87d92fdf400291fef26e
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-06-19 02:29:27 -0400

    Work on big search box on front page and additional styling

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,23 @@
 $(document).on('ready page:load', function () {
   transitionIn($('.feed-item'));
   transitionIn($('.page'));
+  rotateSearchTerms();
 });
+
+function rotateSearchTerms() {
+  $('input#search.big-search').fadeIn(500)
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'orgasm blush').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'hyaluronic acid').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'huda lashes').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'cerave cream').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'green tea').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'strobing powder').dequeue()})
+  .delay(1500)
+  .queue(function() { $(this).attr('placeholder', 'search 737 products')})
+}
