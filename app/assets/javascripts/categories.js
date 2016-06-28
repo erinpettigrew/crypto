c136d1b74d31dc38bbdda39a54a6d6b1cc5f910b
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-06-28 11:25:14 -0400

    Add Javascript sorting by brand, name, rating on category show pages

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -17,28 +17,62 @@ function handleSorting() {
 function sortByBrand() {
   $('.sorts #brand').on('click', function(event) {
     var $sortedProducts = $products
-    $products.hide(200)
     $sortedProducts.sort(function(a,b) {
-      a-b
       var brandA = a.getElementsByClassName('product-brand')[0]
+      brandA = $(brandA).text()
+
       var brandB = b.getElementsByClassName('product-brand')[0]
+      brandB = $(brandB).text()
+
+      var result = 0
+      if (brandA < brandB) result = -1  // sort ascending
+      if (brandA > brandB) result = 1
+      if (brandA === brandB) result = 0
+      return result
     })
-    $sortedProducts.show()
+    $('.category-show').append($sortedProducts)
     event.preventDefault()
   })
 }
 
 function sortByName() {
   $('.sorts #name').on('click', function(event) {
-    $products.hide()
+    var $sortedProducts = $products
+    $sortedProducts.sort(function(a,b) {
+      var nameA = a.getElementsByClassName('product-name')[0]
+      nameA = $(nameA).text()
+
+      var nameB = b.getElementsByClassName('product-name')[0]
+      nameB = $(nameB).text()
+
+      var result = 0
+      if (nameA < nameB) result = -1  // sort ascending
+      if (nameA > nameB) result = 1
+      if (nameA === nameB) result = 0
+      return result
+    })
+    $('.category-show').append($sortedProducts)
     event.preventDefault()
   })
 }
 
-
 function sortByRating() {
   $('.sorts #rating').on('click', function(event) {
-    $products.hide()
+    var $sortedProducts = $products
+    $sortedProducts.sort(function(a,b) {
+      var ratingA = a.getElementsByClassName('rating')[0]
+      ratingA = $(ratingA).text()
+
+      var ratingB = b.getElementsByClassName('rating')[0]
+      ratingB = $(ratingB).text()
+
+      var result = 0
+      if (ratingA < ratingB) result = 1   // sort descending
+      if (ratingA > ratingB) result = -1
+      if (ratingA === ratingB) result = 0
+      return result
+    })
+    $('.category-show').append($sortedProducts)
     event.preventDefault()
   })
 }
