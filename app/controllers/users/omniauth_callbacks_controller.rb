369d8337be626dd2d3a32e6b004865c5c6b8d21f
erinpettigrew <erin.pettigrew@gmail.com>
2016-07-12 13:37:56 -0400

    Fix validations and login flow for both new and existing users, validation errors showing correctly

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
@@ -10,25 +9,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # end
 
     # match to existing user if exists, else create new user
-    # if User.where(email: @user.email).any?
-    #   @existing_user = User.where(email: @user.email).first
-    # else
-    #   @user
-    # end
-    #
-    # # on creating a new user, callbacks send us here
-    # if !@existing_user.nil?
-    #   sign_in_and_redirect @existing_user, :event => :authentication
-    #   set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    # else
+    if User.where(email: @user.email).any?
+      @existing_user = User.where(email: @user.email).first
+    else
+      @user
+    end
+
+    # on creating a new user, callbacks send us here
+    if !@existing_user.nil?
+      sign_in_and_redirect @existing_user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
       if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
+        redirect_to root_path
       end
-    # end
+    end
   end
 
   def failure
