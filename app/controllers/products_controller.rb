d6531ef214b6ad3df3a9d0be6ca9f53028611cdd
erinpettigrew <erin.pettigrew@gmail.com>
2016-07-16 13:17:18 -0400

    Remove question feature from master branch

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,10 +22,10 @@ class ProductsController < ApplicationController
     @rand_categories = Category.all.includes(:products).sample(4)
     @products = Product.take(30).sample(8)
     @brands = Product.brands.sample(4)
-    recent_questions = Question.includes( [user: :avatar]).last(4)
+    # recent_questions = Question.includes( [user: :avatar]).last(4)
     recent_reviews = Review.includes( [user: :avatar], :product).last(8)
     recent_uses = Use.includes( { :user => :avatar }, :product).last(12)
-    @activity = (recent_reviews + recent_uses + recent_questions).sort_by(&:created_at).reverse.first(8)
+    @activity = (recent_reviews + recent_uses).sort_by(&:created_at).reverse.first(8)
     # @popular_products = Product.find(Product.joins(:uses).group('products.id').order("count(*) desc").limit(3).ids)
     @recent_posts = Post.all.order(created_at: :desc).includes(user: :avatar).take(3)
   end
