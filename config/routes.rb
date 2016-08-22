6f501eddb383165875901d27c3781580c18a63c5
Erin Pettigrew <erin.pettigrew@gmail.com>
2016-08-22 16:10:30 -0400

    Update insta links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/availabilities', to: 'availabilities#show'
   get '/insta', to: 'looks#show', :id => '18'
   get '/sparkle', to: 'looks#show', :id => '6'
+  get '/clutch', to: 'looks#show', :id => '20'
   resources :posts
   resources :themes
   resources :skin_types
