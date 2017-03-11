4c0067386c8299fce85e9d5bb39ae60b76c4fa25
Erin Pettigrew <erin.pettigrew@gmail.com>
2017-03-11 14:32:50 -0500

    Added video embedding to posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  def is_video?(post)
+    post.url.include?("youtube.com") ? true : false
+  end
+
+  def render_media(post)
+    if is_video?(post)
+      "<iframe width='560' height='315' src='https://www.youtube.com/embed/#{self.video_id(post)}?autoplay=0&enablejsapi=1' frameborder='0' allowfullscreen></iframe>".html_safe
+    else
+      link_to image_tag(post.image), post.url
+    end
+  end
+
+  def video_id(post)
+    post.url.gsub("https://www.youtube.com/watch?v=", "")
+  end
 end
