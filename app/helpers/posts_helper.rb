ba6478c9be6442bc75e2b45f368ab4cfa3ca4339
Erin Pettigrew <erin.pettigrew@gmail.com>
2017-03-11 17:41:00 -0500

    Add Instagram embed ability to posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,9 +3,15 @@ module PostsHelper
     post.url.include?("youtube.com") ? true : false
   end
 
+  def is_instagram?(post)
+    post.url.include?("instagram.com") ? true: false
+  end
+
   def render_media(post)
     if is_video?(post)
       "<iframe width='368' height='207' src='https://www.youtube.com/embed/#{self.video_id(post)}?autoplay=0&enablejsapi=1' frameborder='0' allowfullscreen></iframe>".html_safe
+    elsif is_instagram?(post)
+      "#{post.url}".html_safe
     else
       link_to image_tag(post.image), post.url
     end
