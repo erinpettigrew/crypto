da906cab94ac961cb31433bd53551410e359c3ef
Erin Pettigrew <erin.pettigrew@gmail.com>
2017-06-03 16:39:18 -0400

    Create admin form for new looks

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,10 +1,47 @@
 class LooksController < ApplicationController
   before_action :set_look, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, except: [:show]
 
   def show
   end
 
+  def new
+    @look = Look.new
+  end
 
+  def create
+    @look = Look.new(look_params)
+
+    respond_to do |format|
+      if @look.save
+        format.html { redirect_to look_path(@look), notice: 'Your look was successfully created.' }
+        format.json { render :show, status: :created, location: @look }
+      else
+        format.html { render :new }
+        format.json { render json: @look.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @look.update(look_params)
+        format.html { redirect_to look_path(@look), notice: 'Your look was successfully updated.' }
+        format.json { render :show, status: :ok, location: @look }
+      else
+        format.html { render :edit }
+        format.json { render json: @look.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @look.destroy
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user), notice: 'Your look was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 
   private
