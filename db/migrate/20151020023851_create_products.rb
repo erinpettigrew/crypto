150a52d4bb39a48f7d3e72eab2bc8469fe13c45b
erinpettigrew <erin.pettigrew@gmail.com>
2017-06-14 11:50:00 -0400

    Clear seeds and edit past migration

--- db/migrate/20151020023851_create_products.rb
+++ db/migrate/20151020023851_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :product_brand
       t.string :product_name
 
       t.timestamps null: false
