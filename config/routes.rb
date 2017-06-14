51228042de66e7ccb2041a6bcfdb77f1f219c381
erinpettigrew <erin.pettigrew@gmail.com>
2017-06-14 11:43:25 -0400

    Remove more brand information from app

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   resources :themes
   resources :applicants
   resources :categories
-  resources :brands
   resources :looks
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", sessions: "sessions" }
   resources :users, :only => [:show, :index, :update] do
