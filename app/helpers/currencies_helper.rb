ec20d5adedf0a0670bfb9c170763e67cb7377a5b
erinpettigrew <erin.pettigrew@gmail.com>
2017-08-19 10:46:12 -0400

    Update price fetcher for several exchanges, format price display in view

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -4,7 +4,35 @@ module CurrenciesHelper
     if price.nil?
       ""
     else
-      "$" + number_with_delimiter(price, :delimiter => ',')
+      display_price = round(price)
+      "$" + number_with_delimiter(display_price, :delimiter => ',')
     end
   end
+
+  def round(price)
+
+    if price.class == Float
+      return price.round(2)
+    end
+
+    if price.class == Fixnum
+      price = price.to_s
+    end
+
+    length = price.length
+    decimal_location = price.index('.')
+    rounded_price = price
+    
+    if decimal_location == nil
+      rounded_price = price + ".00"
+    elsif length - decimal_location > 3
+      rounded_price = price[0...decimal_location + 3]
+    elsif length - decimal_location == 2
+      rounded_price = price + "0"
+    elsif length - decimal_location == 1
+      rounded_price = price + "00"
+    end
+      rounded_price
+    end
+
 end
--- app/services/price_fetcher.rb
+++ app/services/price_fetcher.rb
@@ -8,12 +8,17 @@ class PriceFetcher
   end
 
   def fetch
-    if @exchange.name == "Coinbase"
+    if @exchange.name.downcase == "coinbase" || @exchange.name.downcase == "gdax"
       HTTParty.get("https://api.coinbase.com/v2/prices/" + currency.ticker + "-USD/buy")["data"]["amount"]
+    elsif @exchange.name.downcase == "gemini"
+      HTTParty.get("https://api.gemini.com/v1/pubticker/" + currency.ticker + "usd")["ask"]
+    elsif @exchange.name.downcase == "bitstamp"
+      HTTParty.get("https://www.bitstamp.net/api/v2/ticker/" + currency.ticker + "usd").parsed_response["ask"]
+    elsif @exchange.name.downcase == "cex.io" || @exchange.name.downcase == "cex"
+      HTTParty.get("https://cex.io/api/ticker/" + currency.ticker.upcase + "/USD")["ask"]
     else
       nil
       # HTTParty.get("https://min-api.cryptocompare.com/data/price?fsym=" + currency.ticker + "&tsyms=USD")["USD"]
     end
   end
-
 end
