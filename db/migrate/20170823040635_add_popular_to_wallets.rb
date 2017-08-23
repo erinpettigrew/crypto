668cdbb314116072fd999cdc82926caad8547804
erinpettigrew <erin.pettigrew@gmail.com>
2017-08-23 00:20:43 -0400

    Fix typo in migration

--- db/migrate/20170823040635_add_popular_to_wallets.rb
+++ db/migrate/20170823040635_add_popular_to_wallets.rb
@@ -1,6 +1,6 @@
 class AddPopularToWallets < ActiveRecord::Migration
   def change
     add_column :products, :popular, :boolean
-    add_column :Products, :popular_more, :string
+    add_column :products, :popular_more, :string
   end
 end
